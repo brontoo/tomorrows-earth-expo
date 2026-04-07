1c249e4a51b5c2721d65c0e00b102b9f79127fe7
brontoo <elavukatoo@gmail.com>
2026-04-07 07:33:38 +0000

    fix: add missing /choose-role route to Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,10 +23,16 @@ import SignUp from "./pages/SignUp";
 import { UserWelcomeToast } from "./components/UserWelcomeToast";
 import PageNavigation from "./components/PageNavigation";
 import AuthCallback from "@/pages/AuthCallback";
+import ChooseRole from "./pages/ChooseRole";
 
 function Router() {
   return (
     <Switch>
+    <Route path="/" component={Home} />
+      <Route path="/auth/callback" component={AuthCallback} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/choose-role" component={ChooseRole} />  {/* ← أضف هذا */}
       <Route path="/" component={Home} />
       <Route path="/auth/callback" component={AuthCallback} />
       <Route path="/login" component={Login} />
